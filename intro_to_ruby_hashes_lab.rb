c54f27ffd09dc5356e755cd29bcbac906bda2f0c
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-28 02:40:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{:base => "great"}
+	{:railroads => "great"}
 end
 
 def monopoly_with_second_tier
