2bcfebbec5e414f08450664003fa0ac3b5056f1d
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-28 02:58:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,8 +21,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	{:railroads =>
-		{:rent_in_dollars => {:names => "", :key2 => "", :key3 => "", :key4 => ""}
-
+		{:pieces => "4".to_i}
 	}
 end
 
